d460ed493d034198fa11eca7626f07efe1a552db
Sergey <ryabchenkov@yandex.ru>
2019-11-30 19:37:29 +0300

    chore: update graphics

--- src/constants/graphics.js
+++ src/constants/graphics.js
@@ -1,12 +1,13 @@
 export const Icons = {
-  CALM: require('../../assets/interface/calm.png'),
-  DEPRESSED: require('../../assets/interface/depressed.png'),
-  HAPPY: require('../../assets/interface/happy.png'),
-  SAD: require('../../assets/interface/sad.png'),
-  SWINGS: require('../../assets/interface/swings.png'),
+  CALM: require('../../assets/moods/calm.png'),
+  DEPRESSED: require('../../assets/moods/depressed.png'),
+  HAPPY: require('../../assets/moods/happy.png'),
+  SAD: require('../../assets/moods/sad.png'),
+  SWINGS: require('../../assets/moods/swings.png'),
 };
 
 export const Interface = {
   CHECK_ROUND: require('../../assets/interface/checkRound.png'),
   BACK: require('../../assets/interface/back.png'),
+  EDIT: require('../../assets/interface/edit.png'),
 };
